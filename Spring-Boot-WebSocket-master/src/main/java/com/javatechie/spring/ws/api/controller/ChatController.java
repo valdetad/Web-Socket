d89f9cbffafb8ebb1c8950d879f3d920aec4b7ea
valdetad <valdeta.demiri@hotmail.com>
2024-12-11 12:46:10 +0100

    changed the logic

--- Spring-Boot-WebSocket-master/src/main/java/com/javatechie/spring/ws/api/controller/ChatController.java
+++ Spring-Boot-WebSocket-master/src/main/java/com/javatechie/spring/ws/api/controller/ChatController.java
@@ -1,16 +1,18 @@
 package com.javatechie.spring.ws.api.controller;
 
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
+import com.javatechie.spring.ws.api.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
-import com.javatechie.spring.ws.api.model.ChatMessage;
+import javax.annotation.PreDestroy;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @Controller
 public class ChatController {
+
+	private final ConcurrentHashMap<String, CopyOnWriteArrayList<String>> activeChats = new ConcurrentHashMap<>();
 
 	@MessageMapping("/chat/{chatId}/register")
 	@SendTo("/topic/{chatId}")
@@ -18,14 +20,45 @@ public class ChatController {
 			@Payload ChatMessage chatMessage,
 			SimpMessageHeaderAccessor headerAccessor,
 			@DestinationVariable String chatId) {
-		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-		headerAccessor.getSessionAttributes().put("chatId", chatId);
-		return chatMessage;
+
+		String username = chatMessage.getSender();
+
+		synchronized (activeChats) {
+			if (!activeChats.containsKey(chatId) && !activeChats.isEmpty()) {
+				throw new IllegalArgumentException("Chat ID mismatch. Cannot join this chat.");
+			}
+
+			activeChats.putIfAbsent(chatId, new CopyOnWriteArrayList<>());
+			CopyOnWriteArrayList<String> users = activeChats.get(chatId);
+
+			if (!users.contains(username)) {
+				users.add(username);
+			}
+
+			headerAccessor.getSessionAttributes().put("username", username);
+			headerAccessor.getSessionAttributes().put("chatId", chatId);
+
+			chatMessage.setType(ChatMessage.MessageType.JOIN);
+			return chatMessage;
+		}
 	}
 
 	@MessageMapping("/chat/{chatId}/send")
 	@SendTo("/topic/{chatId}")
-	public ChatMessage sendMessageToChat(@Payload ChatMessage chatMessage) {
+	public ChatMessage sendMessageToChat(
+			@Payload ChatMessage chatMessage,
+			SimpMessageHeaderAccessor headerAccessor,
+			@DestinationVariable String chatId) {
+		String sessionChatId = (String) headerAccessor.getSessionAttributes().get("chatId");
+		if (!chatId.equals(sessionChatId)) {
+			throw new IllegalArgumentException("Chat ID mismatch. Cannot send messages.");
+		}
+
 		return chatMessage;
+	}
+
+	@MessageExceptionHandler(IllegalArgumentException.class)
+	public void handleChatError(IllegalArgumentException ex) {
+		System.err.println("Error: " + ex.getMessage());
 	}
 }
--- Spring-Boot-WebSocket-master/src/main/java/com/javatechie/spring/ws/api/model/ChatMessage.java
+++ Spring-Boot-WebSocket-master/src/main/java/com/javatechie/spring/ws/api/model/ChatMessage.java
@@ -3,14 +3,21 @@ package com.javatechie.spring.ws.api.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name="tbl_chat_message")
 public class ChatMessage {
-
 	@Id
 	@GeneratedValue
 	private Long id;
-
 	private String content;
 	private String sender;
 
@@ -19,37 +26,4 @@ public class ChatMessage {
 	}
 
 	private MessageType type;
-
-	// Getters and Setters
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getSender() {
-		return sender;
-	}
-
-	public void setSender(String sender) {
-		this.sender = sender;
-	}
-
-	public MessageType getType() {
-		return type;
-	}
-
-	public void setType(MessageType type) {
-		this.type = type;
-	}
 }
